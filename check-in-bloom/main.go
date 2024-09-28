ec04437b157ea78426c066bf466fb36783944be5
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2024-09-28 22:45:18 +0330

    add information in bloom
    file check-in-bloom

--- check-in-bloom/main.go
+++ check-in-bloom/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/willf/bloom"
+)
+
+func main() {
+	// Initialize a Bloom filter with a capacity of 1 million and 10 hash functions
+	filter := bloom.New(1_000_000, 10)
+
+	// Simulate a dataset containing one million records
+	for i := 0; i < 1_000_000; i++ {
+		filter.Add([]byte(fmt.Sprintf("item%d", i)))
+	}
+}
