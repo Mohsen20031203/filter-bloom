2e58f8e58242fc04af6d00a1d8dbf9ab6c082a12
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2024-09-30 11:42:40 +0330

    test bloom filter

--- check-in-bloom/main.go
+++ check-in-bloom/main.go
@@ -2,26 +2,49 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/willf/bloom"
 )
 
 func main() {
+	start := time.Now()
+	var countFound int
+	var countNotFound int
+	memStore := make(map[string]int)
 	// Initialize a Bloom filter with a capacity of 1 million and 10 hash functions
-	filter := bloom.New(1_000_000, 10)
-
+	filter := bloom.New(41_000_000, 13)
 	// Simulate a dataset containing one million records
-	for i := 0; i < 1_000_000; i++ {
-		filter.Add([]byte(fmt.Sprintf("item%d", i)))
+	for i := 0; i < 2_000_000; i++ {
+		key := fmt.Sprintf("item%d", i)
+		filter.Add([]byte(key))
+		memStore[key] = i
 	}
+
+	/*n := filter.Test([]byte(fmt.Sprintf("item%d", 1_000_001)))
+	_ = n
+	return*/
 
 	// Check if a few items are present in the filter
-	tests := []string{"item500", "item150000", "non_existent_item"}
-	for _, test := range tests {
+
+	for i := 0; i < 20_000_000; i++ {
+		test := fmt.Sprintf("item%d", i)
 		if filter.Test([]byte(test)) {
-			fmt.Printf("%s is possibly in the set.\n", test)
+			if _, ok := memStore[test]; !ok {
+				fmt.Println("Exists But Failed for ", test)
+				countFound++
+			}
 		} else {
-			fmt.Printf("%s is definitely not in the set.\n", test)
+			if _, ok := memStore[test]; ok {
+				fmt.Println("No Exists But Failed for ", test)
+			}
+			countNotFound++
 		}
 	}
+
+	fmt.Println("Done!!")
+	fmt.Println(countFound)
+	fmt.Println(countNotFound)
+	duration := time.Since(start)
+	fmt.Println(duration)
 }
