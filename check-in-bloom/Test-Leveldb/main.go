e5335577e8b30c67bdffd058e809673662f5c322
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2024-10-09 16:20:48 +0330

    change 20_000_000 in 3_000_000

--- check-in-bloom/Test-Leveldb/main.go
+++ check-in-bloom/Test-Leveldb/main.go
@@ -8,43 +8,42 @@ import (
 )
 
 func main() {
-	start := time.Now()
-	var countFound int
-	var countNotFound int
+	var countFound float64
+	var countNotFound float64
 	memStore := make(map[string]int)
 	// Initialize a Bloom filter with a capacity of 1 million and 10 hash functions
-	filter := bloom.New(41_000_000, 13)
+	filter := bloom.New(65_000_000, 10)
 	// Simulate a dataset containing one million records
-	for i := 0; i < 2_000_000; i++ {
+	for i := 0; i <= 2_000_000; i++ {
 		key := fmt.Sprintf("item%d", i)
 		filter.Add([]byte(key))
 		memStore[key] = i
 	}
 
-	/*n := filter.Test([]byte(fmt.Sprintf("item%d", 1_000_001)))
-	_ = n
-	return*/
-
 	// Check if a few items are present in the filter
+	start := time.Now()
 
-	for i := 0; i < 3_000_000; i++ {
+	var numberAll float64
+	for i := 2_000_000; i < 3_000_000; i++ {
+		numberAll++
 		test := fmt.Sprintf("item%d", i)
 		if filter.Test([]byte(test)) {
 			if _, ok := memStore[test]; !ok {
-				fmt.Println("Exists But Failed for ", test)
 				countFound++
 			}
 		} else {
 			if _, ok := memStore[test]; ok {
 				fmt.Println("No Exists But Failed for ", test)
+				countNotFound++
 			}
-			countNotFound++
 		}
 	}
 
-	fmt.Println("Done!!")
-	fmt.Println(countFound)
-	fmt.Println(countNotFound)
+	fmt.Println("bloom = t | map= f : ", countFound)
+	fmt.Println("bloom = f | map= f : ", countNotFound)
 	duration := time.Since(start)
-	fmt.Println(duration)
+	fmt.Println("All item for check : ", numberAll)
+	fmt.Println("Error percentage‌   : ", (countFound*100)/numberAll)
+	fmt.Println("time check in data : ", duration)
+	fmt.Println("Done!!")
 }
